7eb387402f8b5ede9eb94a4ea4673533195534a6
Sean <12708@coderacademy.edu.au>
2023-06-15 09:32:29 +1000

    nnew form

--- Components/Contactform.js
+++ Components/Contactform.js
@@ -6,8 +6,25 @@ import { MDBInput, MDBTextArea, MDBCheckbox, MDBBtn } from 'mdb-react-ui-kit';
 import Swal from 'sweetalert2'
 import ReCAPTCHA from "react-google-recaptcha";
 import Recaptcha from 'react-google-recaptcha/lib/recaptcha';
+import { useRef } from 'react';
+
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_i3b41wb', 'template_zeri4q7', form.current, 'S6kwfmLLLuI8qHsR0')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
 
   const recaptchaRef = React.createRef();
   const [disable, setDisable] = useState(false);
@@ -30,25 +47,13 @@ export default function Contact() {
 
       <div id="contact" className="flex  justify-center mt-4">
 
-
-
-
-
-
-
-    
-    <form id='form'  className='text-center justify-center mb-12 w-[80vw]' method="POST" action="https://mailthis.to/info@galaxyremovals.sydney" >
+    <form id='form' ref={form} onSubmit={sendEmail} className='text-center justify-center mb-12 w-[80vw]'>
     
 	  
-    {/* <input type="hidden" name="recaptcha_response" id="recaptchaResponse"/> */}
-      {/* <input type="hidden" name="apikey" value="a1b9b123-ed95-4e08-a5bf-a214085534a1"/> */}
       <input type="hidden" name="recaptcha_response" id="recaptchaResponse"></input>
-      <input type="hidden" name="_honeypot"></input>
-      <MDBInput label='Name' name="full_name" v-model='name' wrapperClass='mb-4' />
+      <MDBInput label='Name' name="full_name" v-model='user_name' wrapperClass='mb-4' />
 
-      <MDBInput type='email' label='Email address' name="_replyto" v-model='email' wrapperClass='mb-4' />
-
-     
+      <MDBInput type='email' label='Email address' name="user_email" v-model='email' wrapperClass='mb-4' />
 
       <select name="type" placeholder="Select move type" className="p-2 rounded-2xl border-1 w-full">
         <option></option>
@@ -59,14 +64,14 @@ export default function Contact() {
       </select>
       <div>Select your job type</div> 
       
-      <MDBTextArea wrapperClass='mb-4 mt-4' name="details" label='Job details (location, date, rooms, items, access)' />
+      <MDBTextArea wrapperClass='mb-4 mt-4' name="message" label='Job details (location, date, rooms, items, access)' />
 
       <MDBInput label='Mobile' v-model='Mobile' name="mobile" wrapperClass='mb-4' />
       <ReCAPTCHA sitekey="6LdaZjAmAAAAAPsBlxUgkxR_kH65xVXDZLPsbguB"
                 secretkey="6LdaZjAmAAAAABZYvPKjMz81k0OoYzEREXHDlhQg"
                 onChange={onChange}
       />
-      {disable ? <MDBBtn color='primary' block className='my-4'>
+      {disable ? <MDBBtn color='primary' type="submit" value="Send" block className='my-4'>
         Send
       </MDBBtn> : <></>}
     </form>
